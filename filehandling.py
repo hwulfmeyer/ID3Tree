59a30c100efe5127a5bf6f1c32eabb4183f2055c
wulfihm <hans-martin.wulfmeyer@st.ovgu.de>
2017-11-10 21:19:25 +0100

    reading the specifications of our data done

--- filehandling.py
+++ filehandling.py
@@ -1,8 +1,30 @@
 """
 This file is for the methods concerning everything from file reading to file writing
 """
+import re
 
 
-def mult(x, y):
-    return x*y
+# function to read class names & attributes
+def read_data_names(filepath):
+    with open(filepath, "r") as f:
+        lines = f.read().splitlines()
+
+    classes = re.sub(r'^' + re.escape("class values: "), '', lines.pop(0))
+    classes = classes.split(", ")
+    attributes = re.sub(r'^' + re.escape("attributes: "), '', lines.pop(0))
+    attributes = attributes.split(", ")
+    attributes_values = []
+
+    for i in range(0, len(attributes)):
+        values = re.sub(r'^' + re.escape(attributes[i] + ": "), '', lines.pop(0)).split(", ")
+        attributes_values.append(values)
+
+    return classes, attributes, attributes_values
+
+
+# function to read the actual data
+def read_data(filepath):
+    # open file
+    file = open(filepath, "r")
+
 
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 """
 This file is for executing everything together
 """
-
-import filehandling as fh
+import filehandling as fiha
 import id3algorithm as id3
 
-print(fh.mult(10, 20))
 
-print("Hello world")
+filepathnames = "datasets/cardaten/carnames.txt"
+filepathdata = "datasets/cardaten/car.data"
 
 
+classes, attributes, attribute_values = fiha.read_data_names(filepathnames)
+
+print(classes)
+print(attributes)
+print(attribute_values)
